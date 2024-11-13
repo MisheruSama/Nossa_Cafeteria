c82955070fbb9275f9bebc18c2c5c766e73f0de4
MisheruSama <michelgermano010@outlook.com>
2024-11-13 04:44:40 -0300

    Mudança nos controllers

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/AcompanhamentoAcaiController.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/AcompanhamentoAcaiController.java
@@ -3,15 +3,15 @@ package com.NossaCafeteria.Cardapio.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.NossaCafeteria.Cardapio.DAO.AcompanhamentoAcaiDAO;
 import com.NossaCafeteria.Cardapio.Model.AcompanhamentoAcai;
 
-@RestController
+@Controller
 public class AcompanhamentoAcaiController {
 @Autowired
 private AcompanhamentoAcaiDAO acompanhamento;
--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/DocesControllers.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/DocesControllers.java
@@ -1,15 +1,15 @@
 package com.NossaCafeteria.Cardapio.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import com.NossaCafeteria.Cardapio.DAO.DocesDAO;
 import com.NossaCafeteria.Cardapio.Model.Doces;
 
-@RestController
+@Controller
 public class DocesControllers {
 @Autowired
 private DocesDAO candy;

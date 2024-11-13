ca8a5e4b74aac924c07416691ba2f414dc0519f1
MisheruSama <michelgermano010@outlook.com>
2024-11-13 04:41:05 -0300

    Modificaçao do Controller

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
@@ -3,15 +3,16 @@ package com.NossaCafeteria.Cardapio.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.NossaCafeteria.Cardapio.Model.Salgados;
 import com.NossaCafeteria.Cardapio.DAO.SalgadosDAO;
 
-@RestController
+@Controller
 public class SalgadosControllers {
 
 @Autowired

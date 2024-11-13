00dc05efb300c699c74a292cc0bdb789ee0b97d5
MisheruSama <michelgermano010@outlook.com>
2024-11-13 08:59:56 -0300

    AAAA

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
@@ -3,23 +3,28 @@ package com.NossaCafeteria.Cardapio.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.NossaCafeteria.Cardapio.DAO.SalgadosDAO;
 import com.NossaCafeteria.Cardapio.Model.Salgados;
 
-@RestController
-@RequestMapping("/Salgado")
+@Controller
 public class SalgadosControllers {
 
 @Autowired
 private SalgadosDAO salgados;
 
+@GetMapping("/Salgado")
+public String indexSalgado(){
+    return "salgados";
+}
+
 @GetMapping("/Salgado/api")
+@ResponseBody
 public List<Salgados> listarSalgados(){
     return salgados.obterTodos();
 }

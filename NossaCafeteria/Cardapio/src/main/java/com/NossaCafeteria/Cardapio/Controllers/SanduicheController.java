057eba10d0bfcdcb9e45a18c7e38bf2b50cbf792
MisheruSama <michelgermano010@outlook.com>
2024-11-21 18:33:24 -0300

    Uso do DeleteMapping

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SanduicheController.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SanduicheController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.NossaCafeteria.Cardapio.DAO.SanduicheDAO;
 import com.NossaCafeteria.Cardapio.Model.Sanduiche;
 @Controller
@@ -30,8 +31,8 @@ public String salvar(@RequestBody Sanduiche sanduiches){
     sanduiche.incluir(sanduiches);
     return "redirect:/salgados.html";
 }
-@PostMapping("/sanduiches/excluir")
-public String excluir(@RequestBody Integer id){
+@DeleteMapping("/sanduiches/{id}")
+public String excluir(@PathVariable Integer id){
     sanduiche.excluir(id);
     return "redirect:/salgados.html";
 }

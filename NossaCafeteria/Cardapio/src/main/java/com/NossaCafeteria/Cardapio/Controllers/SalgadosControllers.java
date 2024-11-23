1686b579a279fca3ae8ac5d29bc1f0b007015567
MisheruSama <michelgermano010@outlook.com>
2024-11-23 16:11:35 -0300

    boa boa

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,8 +34,8 @@ public String salvarSalgado(@RequestBody Salgados salgado){
     salgados.incluir(salgado);
     return "redirect:/salgados.html";
 }
-@PostMapping("/salgados/excluir")
-public String excluirSalgado(@RequestBody Integer id){
+@PostMapping("/salgados/{id}")
+public String excluirSalgado(@PathVariable Integer id){
     salgados.excluir(id);
     return "redirect:/salgados.html";
 }

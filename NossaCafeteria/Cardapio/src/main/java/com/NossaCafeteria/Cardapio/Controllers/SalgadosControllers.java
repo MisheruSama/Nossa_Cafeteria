052bca88ad288086d534733f64d2ddb9862b2708
MisheruSama <michelgermano010@outlook.com>
2024-11-25 00:04:52 -0300

    b

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SalgadosControllers.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +34,7 @@ public String salvarSalgado(@RequestBody Salgados salgado){
     salgados.incluir(salgado);
     return "redirect:/salgados.html";
 }
-@DeleteMapping("/salgados/{id}")
+@PostMapping("/salgados/{id}")
 public String excluirSalgado(@PathVariable Integer id){
     salgados.excluir(id);
     return "redirect:/salgados.html";
--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SanduicheController.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SanduicheController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +31,7 @@ public String salvar(@RequestBody Sanduiche sanduiches){
     sanduiche.incluir(sanduiches);
     return "redirect:/salgados.html";
 }
-@DeleteMapping("/sanduiches/{id}")
+@PostMapping("/sanduiches/{id}")
 public String excluir(@PathVariable Integer id){
     sanduiche.excluir(id);
     return "redirect:/salgados.html";

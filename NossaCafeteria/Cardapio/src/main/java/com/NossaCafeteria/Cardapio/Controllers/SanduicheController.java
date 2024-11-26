3d089aa44a12fe552f4db5ce925a4778d01bc29c
MisheruSama <michelgermano010@outlook.com>
2024-11-26 03:14:20 -0300

    vamo la

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SanduicheController.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/SanduicheController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +32,7 @@ public String salvar(@RequestBody Sanduiche sanduiches){
     sanduiche.incluir(sanduiches);
     return "redirect:/salgados.html";
 }
-@PostMapping("/sanduiches/excluir/{id}")
+@DeleteMapping("/sanduiches/excluir/{id}")
 public String excluir(@PathVariable Integer id){
     sanduiche.excluir(id);
     return "redirect:/salgados.html";

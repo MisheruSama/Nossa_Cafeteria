b8250290df9a5aa72753be2d47fa8a9ee7d8dcba
MisheruSama <michelgermano010@outlook.com>
2024-11-27 01:18:19 -0300

    graças a Deus finalizamos

--- NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/DocesControllers.java
+++ NossaCafeteria/Cardapio/src/main/java/com/NossaCafeteria/Cardapio/Controllers/DocesControllers.java
@@ -3,8 +3,12 @@ package com.NossaCafeteria.Cardapio.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,10 +35,14 @@ public String salvarDoces(@RequestBody Doces doce){
     candy.incluir(doce);
     return "redirect:/doces.html";
 }
-@PostMapping("/doces/excluir")
-public String excluir(@RequestBody Integer id){
+@DeleteMapping("/doces/excluir/{id}")
+public ResponseEntity<String> excluir(@PathVariable Integer id){
+    try{
     candy.excluir(id);
-    return "redirect:/doces.html";
+    return ResponseEntity.ok("Item excluido com sucesso");
+    } catch(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao excluir o item");
+    }
 }
 @PostMapping("/doces/alterar")
 public String editarDoces(@RequestBody Doces doce){
